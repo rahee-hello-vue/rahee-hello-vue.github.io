9a40cc014f0ec960dd78d57b15f3a43685f713a4
raheego <raae517@gmail.com>
2024-01-31 17:17:07 +0900

    모든 내역 출력

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup>
 import { ref } from 'vue'
-import category from '@/assets/category.json'
+import mainMovie from '@/assets/movie.json'
 
-const parentMessage = ref('Parent')
-const items = ref([
-    { message: 'Foo' },
-    { message: 'Bar' }
-])
-const categories = ref(category)
+const movie  = ref(mainMovie.movies)
 </script>
 
 <template>
-<li v-for="(category, index) in categories" :key="index">
-    {{ category.name }} - {{ category.code }}
-</li>
+<ul>
+	<li v-for="(mv, index) in movie" :key="index">
+		 <p class="title"> 영화제목 [ {{ mv.title_kor }} ] </p>
+		<p><img :src="mv.poster_url" /></p>
+		<p>{{ mv.genre }}</p>
+		<p>{{ mv.summary }}</p> 
+	</li>
+	
+</ul>
 </template>
-
+<style scoped>
+li {margin: 30px 0}
+.title { font-size: 18px; margin-bottom: 20px; font-weight: bold}
+li p:last-child{margin-top:15px;}
+</style>
