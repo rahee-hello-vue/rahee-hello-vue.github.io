1237fdf0e25f5d8d10aee3b0406d73e7f3d5266d
raheego <raae517@gmail.com>
2024-01-31 17:35:40 +0900

    특정 단어 : 드라마만 출력

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,33 @@
 import { ref } from 'vue'
 import mainMovie from '@/assets/movie.json'
 
-const movie  = ref(mainMovie.movies)
+const movies = ref(mainMovie.movies)
 </script>
 
 <template>
-<ul>
-	<li v-for="(mv, index) in movie" :key="index">
-		 <p class="title"> 영화제목 [ {{ mv.title_kor }} ] </p>
-		<p><img :src="mv.poster_url" /></p>
-		<p>{{ mv.genre }}</p>
-		<p>{{ mv.summary }}</p> 
-	</li>
-	
-</ul>
+  <ul>
+	<template  v-for="(movie, index) in movies" :key="index">
+    <li v-if="movie.genre.includes('드라마')">
+      <p class="title"> 영화제목 [ {{ movie.title_kor }} ] </p>
+      <p><img :src="movie.poster_url" /></p>
+      <p>{{ movie.genre }}</p>
+      <p>{{ movie.summary }}</p>
+    </li>
+	</template>
+  </ul>
 </template>
+
 <style scoped>
-li {margin: 30px 0}
-.title { font-size: 18px; margin-bottom: 20px; font-weight: bold}
-li p:last-child{margin-top:15px;}
+li {
+  margin: 30px 0;
+}
+.title {
+  font-size: 18px;
+  margin-bottom: 20px;
+  font-weight: bold;
+}
+li p:not(.title) {
+  margin-top: 15px;
+}
 </style>
+
