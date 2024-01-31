e9401c0ab4ea2a0eeaa3468fe0656e57ff79f281
raheego <raae517@gmail.com>
2024-01-31 12:09:37 +0900

    var

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,8 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-
-//초기값
-const count = ref({
-	cnt:0,
-	name: "goh"
-})
-
-//
-function increment(){
-	count.value.cnt++
-	count.value.name = count.value.name.split('').reverse().join('')
-}
-
-
-// lifecycle
-onMounted(() => console.log(`The initial count is ${count.value}`) )
-
+import { ref } from 'vue'
+const message = ref('hello world')
 </script>
 
 <template>
-	<button @click="increment">count is : {{ count.cnt }}   {{ count.name }}</button>
+	<h1>{{message}}</h1>
 </template>
-
-<style scoped>
-button{
-	font-weight: bold;
-}
-</style>
