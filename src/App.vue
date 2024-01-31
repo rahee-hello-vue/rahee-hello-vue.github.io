23da9c9898b38cde045e23a4d6f524d634859c5b
raheego <raae517@gmail.com>
2024-01-31 14:52:31 +0900

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-const awesome = ref(true)
 
-
-function toggleAwesome(v){
-  awesome.value = !v
-}
-
-
-function isAwesome(){
-  return awesome.value
-}
-
+const parentMessage = ref('Parent')
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
 </script>
 
 <template>
-	<button @click="toggleAwesome(v)">toggle</button>
-	<br/>
-	<h1 v-if="isAwesome()">Vue is awesome!</h1>
-	<h1 v-else>No</h1>
-
-	<h2 v-if="isAwesome">gg</h2>
-	<h4 v-show="isAwesome()">wow</h4>
+<li v-for="(item, index) in items">
+        {{ parentMessage }} - {{ index }} - {{ item.message }}
+</li>
 </template>
-
