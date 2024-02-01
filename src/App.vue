4d8d2cdda4d549d21e14e3e33ee786e15093c9e2
raheego <raae517@gmail.com>
2024-02-01 17:53:12 +0900

    props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { ref } from 'vue'
 import Child from './components/Child.vue'
+
+const pocketMoney = ref('부모 컴포넌트로부터 <하트>를 전달받았어요');
 </script>
 
 <template>
-	<Child/>
+	<Child :pocketMoney = "pocketMoney"/>
 </template>
 
 <style scoped>
