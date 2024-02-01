e9a3f3adf324447375c300ae93eac57227b3a93f
raheego <raae517@gmail.com>
2024-02-01 10:04:27 +0900

    study

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,19 @@
 <script setup>
-import { ref } from 'vue'
-import mainMovie from '@/assets/movie.json'
+import { reactive, computed , ref }  from 'vue'
 
-const movies = ref(mainMovie.movies)
+const author = ref({
+	name : 'judy',
+	books: [1,2,3]
+});
+
 </script>
 
 <template>
-  <ul>
-	<template  v-for="(movie, index) in movies" :key="index">
-    <li v-if="movie.genre.includes('드라마')">
-      <p class="title"> 영화제목 [ {{ movie.title_kor }} ] </p>
-      <p><img :src="movie.poster_url" /></p>
-      <p>{{ movie.genre }}</p>
-      <p>{{ movie.summary }}</p>
-    </li>
-	</template>
-  </ul>
+	<p>has published books:</p>
+	<p> {{ author.books.length > 2 ? 'yes' : 'no' }} </p>
 </template>
 
 <style scoped>
-li {
-  margin: 30px 0;
-}
-.title {
-  font-size: 18px;
-  margin-bottom: 20px;
-  font-weight: bold;
-}
-li p:not(.title) {
-  margin-top: 15px;
-}
 </style>
+
 
