ba11192c5892aeb08630c57a7929a4aa26c09ae9
raheego <raae517@gmail.com>
2024-02-02 11:46:22 +0900

    use api

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,26 @@
 <script setup>
-import { ref } from 'vue'
+import { ref , watch } from 'vue'
 
-const show = ref(true)
-const list = ref([1,2,3])
+const todoId = ref(1);
+const todoData = ref(null);
 
-
-const listToggle = () => {
-	 document.querySelector('.text').innerHTML = ''
-	show.value = !show.value;
+async function fetchData(){
+	todoData.value = null;
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+	)
+	todoData.value = await res.json();
 }
 
-const listAdd = () => {
-	const num = list.value.length + 1; 
-	const addItem = list.value.push(num);
-	 document.querySelector('.text').innerHTML= `${addItem}가 추가 되었습니다`
-
-}
-
-const listRemove = () => {
-	if ( list.value.length > 0 ) {
-	const removeItem = list.value.pop();
-		 document.querySelector('.text').innerHTML = `${removeItem}가 제거되었습니다`	
-	} else { 
-		alert('더 이상 삭제할 수 없습니다')
-	}
-}	
-
-const listReverse = () => {
-	 document.querySelector('.text').innerHTML = ''
-	const reverse = list.value.reverse();
-	 document.querySelector('.text').innerHTML  = reverse.value.join(', ');
-}
-
-
+fetchData();
 </script>
 
 <template>
-	<button @click="listToggle">List랜더링on/off</button>
-	<button @click="listAdd">List 추가 </button>
-	<button @click="listRemove">List 제거 </button>
-	<button @click="listReverse">List 뒤집기 </button>
+	<p> Todo id : {{ todoId }} </p>
+	 <button @click="todoId++" :disabled="!todoData">Fetch next todo</button>
+	<p v-if="!todoData">Loading</p>
+	<pre v-else> {{ todoData }} </pre>
 
-	<p class="text"></p>
-
-	<ul v-if="show">
-		<li v-for="item of list">{{ item }} </li>
-	</ul>
-
-	<p v-else-if="list.length">list is not empty, but hidden</p>
-	<p v-else>list is empty</p>
 </template>
 
 <style scoped>
